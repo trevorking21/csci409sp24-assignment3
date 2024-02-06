97f9072fdc8a860a4555754e2d6d946a0ad330f0
Trevor King <tgking@coastal.edu>
2024-02-06 12:08:32 -0500

    Created Runways Model

--- api/core/models.py
+++ api/core/models.py
@@ -11,3 +11,20 @@ class Airport(models.Model):
 
     def __str__(self):
         return self.name
+
+class Airline(models.Model):
+    name = models.CharField(max_length=255)
+    airline_code = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.name
+    
+class Runways(models.Model):
+    runway_number = models.IntegerField()
+    runway_designation = models.CharField(max_length=1,choices=(('L','Left'), ('C','Center'), ('R', 'Right'), ('N', 'None')))
+    length = models.IntegerField()
+    width = models.IntegerField()
+    airport = models.ForeignKey(Airport, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.runway_number + self.runway_designation
--- api/core/tests/test_models.py
+++ api/core/tests/test_models.py
@@ -6,6 +6,18 @@ from django.test import TestCase
 
 from core import models
 
+from datetime import datetime
+
+def create_airport():
+    return models.Airport.objects.create(
+        name="Myrtle Beach International",
+        airport_code="MYR",
+        address="1100 Jetport Rd",
+        city="Myrtle Beach",
+        state="SC",
+        zip_code="29577"
+    )
+
 class ModelTests(TestCase):
 
     def test_create_airport(self):
@@ -20,3 +32,44 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(airport), airport.name)
+
+def test_create_airline(self):
+        """Test Creating an Airline is Successful."""
+        airline = models.Airline.objects.create(
+            name="Delta Airlines",
+            airline_code="DL"
+        )
+
+        self.assertEqual(str(airline), airline.name)
+
+def test_create_runway(self):
+        """Test Creating a Runway is Successful."""
+        runway = models.Runway.objects.create(
+            airport = create_airport(),
+            runway_number = 18,
+            runway_designation = "N",
+            length=5000,
+            width=5000
+        )
+
+        self.assertEqual(str(runway), str(runway.runway_number)+runway.runway_designation)
+
+
+def test_create_flight(self):
+        """Test Creating a Flight is Successful."""
+        airline = models.Airline.objects.create(
+            name='Delta Airlines',
+            airline_code='DL'
+        )
+
+        flight = models.Flight.objects.create(
+            origin = create_airport(),
+            destination = create_airport(),
+            airline = airline,
+            flight_number = 1954,
+            departure = datetime.now(),
+            arrival = datetime.now(),
+            aircraft_type = 'B747',
+        )
+
+        self.assertEqual(str(flight), flight.airline.airline_code + str(flight.flight_number))
