bab354fd9384dd7a67fb302c0c66e5e731247a63
Trevor King <tgking@coastal.edu>
2024-02-06 11:39:24 -0500

    Created airport model

--- api/core/models.py
+++ api/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Airport(models.Model):
+    name = models.CharField(max_length=255)
+    airport_code = models.CharField(max_length=3)
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=2)
+    zip_code = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.name
