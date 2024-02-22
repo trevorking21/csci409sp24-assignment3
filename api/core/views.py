43f58fba0b17968e4afbc879b113b91fe5faf0de
Trevor King <tgking@coastal.edu>
2024-02-22 13:13:10 -0500

    Created Viewsets

--- api/core/views.py
+++ api/core/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Airline
+from .models import Airport
+from .models import Runway
+from .models import Flight
+from .serializers import AirlineSerializer
+from .serializers import AirportSerializer
+from .serializers import RunwaySerializer
+from .serializers import FlightSerializer
 
 # Create your views here.
+
+class AirlineViewSet(viewsets.ModelViewSet):
+    queryset = Airline.objects.all()
+    serializer_class = AirlineSerializer
+
+class AirportViewSet(viewsets.ModelViewSet):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
+
+class RunwayViewSet(viewsets.ModelViewSet):
+    queryset = Runway.objects.all()
+    serializer_class = RunwaySerializer
+
+class FlightViewSet(viewsets.ModelViewSet):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
